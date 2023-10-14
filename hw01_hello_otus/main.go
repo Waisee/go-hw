42f6271237e5fe0e6ff3e3cd5225cab77cfda1dd
Danil <danil.baluev@rt.ru>
2023-10-14 18:11:25 +0300

    Second part completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,64 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"strconv"
-	"strings"
-	"unicode"
+
+	"golang.org/x/example/hello/reverse"
 )
 
-var ErrInvalidString = errors.New("invalid string")
-
 func main() {
-	// fmt.Println(reverse.String("Hello, OTUS!"))
-	var b strings.Builder
-	backslash := "\\"
-	r := []rune(`qwe\4\5`)
-
-	for i := 0; i < len(r); i++ {
-		if string(r[i]) == backslash {
-			if string(r[i-1]) == backslash {
-				if i+1 != len(r) {
-					if digit, err := strconv.Atoi(string(r[i+1])); err == nil {
-						b.WriteString(strings.Repeat(string(r[i]), digit))
-					} else {
-						b.WriteRune(r[i])
-					}
-				} else {
-					b.WriteRune(r[i])
-				}
-			}
-		}
-		if unicode.IsDigit(r[i]) {
-			if string(r[i-1]) == backslash {
-				if i+1 != len(r) {
-					if digit, err := strconv.Atoi(string(r[i+1])); err == nil {
-						b.WriteString(strings.Repeat(string(r[i]), digit))
-					} else {
-						b.WriteRune(r[i])
-					}
-				} else {
-					b.WriteRune(r[i])
-				}
-			} else {
-				if i == 0 || unicode.IsDigit(r[i+1]) {
-					fmt.Println(ErrInvalidString)
-					return
-				}
-			}
-		}
-		if unicode.IsLetter(r[i]) || unicode.IsSpace(r[i]) {
-			if i+1 != len(r) {
-				if digit, err := strconv.Atoi(string(r[i+1])); err == nil {
-					b.WriteString(strings.Repeat(string(r[i]), digit))
-				} else {
-					b.WriteRune(r[i])
-				}
-			} else {
-				b.WriteRune(r[i])
-			}
-		}
-	}
-	fmt.Println(b.String())
+	fmt.Println(reverse.String("Hello, OTUS!"))
 }
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,12 +11,47 @@ var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(str string) (string, error) {
 	var b strings.Builder
+	backslash := "\\"
+	escape := false
+
 	r := []rune(str)
 
 	for i := 0; i < len(r); i++ {
+		if string(r[i]) == backslash {
+			if escape {
+				if i+1 != len(r) {
+					if digit, err := strconv.Atoi(string(r[i+1])); err == nil {
+						b.WriteString(strings.Repeat(string(r[i]), digit))
+					} else {
+						b.WriteRune(r[i])
+					}
+				} else {
+					b.WriteRune(r[i])
+				}
+				escape = false
+			} else {
+				escape = true
+				continue
+			}
+		}
 		if unicode.IsDigit(r[i]) {
-			if i == 0 || unicode.IsDigit(r[i+1]) {
-				return "", ErrInvalidString
+			if escape {
+				if i+1 != len(r) {
+					if digit, err := strconv.Atoi(string(r[i+1])); err == nil {
+						b.WriteString(strings.Repeat(string(r[i]), digit))
+					} else {
+						b.WriteRune(r[i])
+					}
+				} else {
+					b.WriteRune(r[i])
+				}
+				escape = false
+			} else {
+				if i+1 != len(r) {
+					if i == 0 || unicode.IsDigit(r[i+1]) {
+						return "", ErrInvalidString
+					}
+				}
 			}
 		}
 		if unicode.IsLetter(r[i]) || unicode.IsSpace(r[i]) {
@@ -30,6 +65,9 @@ func Unpack(str string) (string, error) {
 				b.WriteString(string(r[i]))
 			}
 		}
+	}
+	if escape {
+		return "", ErrInvalidString
 	}
 	return b.String(), nil
 }
