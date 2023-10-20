abe1482b6e821247c0cb9032e01c3ef7fd08e115
Danil <danil.baluev@rt.ru>
2023-10-20 22:37:50 +0300

    Hw2 without *

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,66 +11,23 @@ var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(str string) (string, error) {
 	var b strings.Builder
-	escape := false
-
 	r := []rune(str)
 
 	for i := 0; i < len(r); i++ {
-		switch getType(r[i]) {
-		case "backslash":
-			if escape {
-				if i+1 != len(r) {
-					checkAndWrite(&b, r[i], r[i+1])
-				} else {
-					b.WriteRune(r[i])
-				}
-				escape = false
-			} else {
-				escape = true
-				continue
-			}
-		case "digit":
-			if escape {
-				if i+1 != len(r) {
-					checkAndWrite(&b, r[i], r[i+1])
-				} else {
-					b.WriteRune(r[i])
-				}
-				escape = false
-			} else if i+1 != len(r) && (i == 0 || unicode.IsDigit(r[i+1])) {
+		count := 1
+		if unicode.IsDigit(r[i]) {
+			if i == 0 || unicode.IsDigit(r[i+1]) {
 				return "", ErrInvalidString
 			}
-		case "char":
-			if escape {
-				return "", ErrInvalidString
+		}
+		if unicode.IsLetter(r[i]) || unicode.IsSpace(r[i]) {
+			if i < len(r)-1 {
+				if digit, err := strconv.Atoi(string(r[i+1])); err == nil {
+					count = digit
+				}
 			}
-			if i+1 != len(r) {
-				checkAndWrite(&b, r[i], r[i+1])
-			} else {
-				b.WriteRune(r[i])
-			}
+			b.WriteString(strings.Repeat(string(r[i]), count))
 		}
 	}
 	return b.String(), nil
-}
-
-func getType(r rune) string {
-	switch {
-	case string(r) == "\\":
-		return "backslash"
-	case unicode.IsDigit(r):
-		return "digit"
-	case unicode.IsLetter(r) || unicode.IsSpace(r):
-		return "char"
-	default:
-		return ""
-	}
-}
-
-func checkAndWrite(b *strings.Builder, currentRune rune, nextRune rune) {
-	if digit, err := strconv.Atoi(string(nextRune)); err == nil {
-		b.WriteString(strings.Repeat(string(currentRune), digit))
-	} else {
-		b.WriteRune(currentRune)
-	}
 }
